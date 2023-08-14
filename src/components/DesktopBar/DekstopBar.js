66549137fefcf2d85746c5e31984b81ff63bd52b
William Nyunt <william.nyunt@7dots.co.uk>
2023-08-14 08:55:10 +0100

    debuggeed redux error

--- src/components/DesktopBar/DekstopBar.js
+++ src/components/DesktopBar/DekstopBar.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import TimeWidget from "../Widgets/TimeWidget";
 import '../../scss/DesktopBar/DekstopBar.scss';
+import { useSelector, useDispatch } from 'react-redux';
+import { setTheme } from '../../slices/theme';
+
 const DesktopBar = () => {
+
+    const theme = useSelector((state) => state.theme.theme);
+    const dispatch = useDispatch();
+
+    const toggleTheme = () => {
+        const newTheme = theme === 'light-theme' ? 'dark-theme' : 'light-theme';
+        dispatch(setTheme(newTheme));
+      };
+
     return (
         <div className="desktop-bar">
             <TimeWidget />
+            <button onClick={toggleTheme}>Toggle Theme</button>
+             <div className={theme === 'light-theme' ? 'light-theme' : 'dark-theme'}>
+            </div>
         </div>
     )
 }
