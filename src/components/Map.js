f7e6cb0afcf9d39b78332b9b00ba6d8a0d70e1f1
William Nyunt <william.nyunt@7dots.co.uk>
2023-08-15 09:05:06 +0100

    Dark mode map

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,10 +1,9 @@
-import React, {Component} from 'react';
 import 'leaflet/dist/leaflet.css'
 import L from 'leaflet'
 import {MapContainer, TileLayer} from 'react-leaflet'
 import '../scss/Map.scss';
 import DesktopBar from "./DesktopBar/DekstopBar";
-
+import { useSelector} from 'react-redux';
 
 delete L.Icon.Default.prototype._getIconUrl;
 
@@ -14,8 +13,12 @@ L.Icon.Default.mergeOptions({
     shadowUrl: require('leaflet/dist/images/marker-shadow.png')
 });
 
-class Map extends Component {
-    render() {
+
+function Map () {
+    const theme = useSelector((state) => state.theme.theme);
+
+        
+    
         return (
             <MapContainer className="full-height-map"
                 center={[10, 120]}
@@ -25,12 +28,11 @@ class Map extends Component {
                 scrollWheelZoom={true}>
                 <TileLayer
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Tiles style by <a href="https://www.hotosm.org/" target="_blank">Humanitarian OpenStreetMap Team</a> hosted by <a href="https://openstreetmap.fr/" target="_blank">OpenStreetMap France</a>'
-                    url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+                    url={theme === 'dark-theme' ?  ('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png') : ('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png')}
                 />
                 <DesktopBar/>
             </MapContainer>
         );
-    }
 }
 
 export default Map;
