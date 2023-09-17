4282831878ee42ac0e6bf624b5417a9904f26454
William Nyunt <william.nyunt@7dots.co.uk>
2023-09-17 19:51:50 +0100

    Modal tooltips added

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -9,10 +9,16 @@ const Modal = (props) => {
         dispatch(setModal(false))
     }
 
+    const modalOpenHandler = () => {
+        dispatch(setModal(true))
+    }
+
+
     return (
-        <div className={`modal ${props.open && '-open'}`}>
-            <span className='modal-exit' onClick={modalCloseHandler}>X</span>
+        <div className={`modal ${props.open ? '-open' : '-closed'}`}>
+            <span className='modal-exit' onClick={modalCloseHandler}></span>
             <h3 className='modal-title'>Modal title</h3>
+            <span className='modal-open-tooltip' data-tooltip='Add new nodes' onClick={modalOpenHandler}></span>
         </div>
     )
 }
