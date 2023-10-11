b00b650969f82465219f6037ac18a273ab988149
William Nyunt <william.nyunt@7dots.co.uk>
2023-10-11 16:56:37 +0100

    Add trip control

--- src/components/Modal/AddTripForm.js
+++ src/components/Modal/AddTripForm.js
@@ -1,6 +1,8 @@
 import React from 'react'
+import { useState } from 'react'
 
 export default function AddTripForm() {
+  const [name, setName] = useState('')
 
   const addTripSubmitListener = (e) => {
     e.preventDefault()
@@ -11,7 +13,7 @@ export default function AddTripForm() {
     <form className='flex flex-col gap-y-4'>
         <div>
             <label forhtml='name' classname='block mb-2 text-sm font-medium text-gray-900 dark:text-white'>Name</label>
-            <input type='text' className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500' name='name'/>
+            <input type='text' value={name} onChange={e => setName(e.target.value)} className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500' name='name'/>
         </div>
 
         <button type='submit' className="btn btn-dm-white rounded-lg" onSubmit={addTripSubmitListener}>Add</button>
