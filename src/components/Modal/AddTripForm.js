3788207b3de1d4fe0b4db3059d4f83be3c9b2adf
William Nyunt <william.nyunt@7dots.co.uk>
2023-10-11 16:51:12 +0100

    Trip modal multi-layer logic

--- src/components/Modal/AddTripForm.js
+++ src/components/Modal/AddTripForm.js
@@ -1,7 +1,20 @@
 import React from 'react'
 
 export default function AddTripForm() {
+
+  const addTripSubmitListener = (e) => {
+    e.preventDefault()
+
+  }
+
   return (
-    <div>AddTripForm</div>
+    <form className='flex flex-col gap-y-4'>
+        <div>
+            <label forhtml='name' classname='block mb-2 text-sm font-medium text-gray-900 dark:text-white'>Name</label>
+            <input type='text' className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500' name='name'/>
+        </div>
+
+        <button type='submit' className="btn btn-dm-white rounded-lg" onSubmit={addTripSubmitListener}>Add</button>
+    </form>
   )
 }
--- src/components/Modal/TripModal.js
+++ src/components/Modal/TripModal.js
@@ -2,55 +2,41 @@ import './TripModal.scss';
 import { useDispatch } from 'react-redux';
 import { setTripModal } from '../../slices/tripModal';
 import { RxCross2 } from "react-icons/rx";
-import { useGetTripsQuery } from 'src/slices/trip';
-import { Skeleton } from '@mui/material';
 import TripTable from './TripTable';
 import { useState } from 'react';
+import AddTripForm from './AddTripForm';
 
 export const TripModal = () => {
+    const [ addTripFormOpen, setAddTripFormOpen ] = useState(false);
     const dispatch = useDispatch();
 
     const modalCloseHandler = () => {
         dispatch(setTripModal(false))
     }
 
-    const modalOpenHandler = () => {
-        dispatch(setTripModal(true))
-    }
-    // const trips = useSelector(getAllTrips);
-
-    const { data, error, isLoading} = useGetTripsQuery('iSojVlIFhQh631bOJmvM6HLZV1I2');
-
     const addTripOnClickListener = () => {
-        console.log('clicked')
+        setAddTripFormOpen(true);
     }
 
-    const { addTripFormOpen, setAddTripFormOpen } = useState(false);
+    const backTripModalListener = () => {
+        setAddTripFormOpen(false);
+    }
 
     return (
         <div className='trip-modal'>
             <RxCross2 className='trip-modal__exit'  onClick={modalCloseHandler} />
             <h3 className='trip-modal__title'>Trips</h3>
             <div className='trip-modal__content'>
-            { isLoading &&  (
-            <div className='flex flex-col gap-y-4'>
-                <Skeleton sx={{ bgcolor: 'grey.900' }} variant="rectangular" width={100} height={48} />
-                <Skeleton sx={{ bgcolor: 'grey.700', fontSize: '18px', width: '80%'}} className="mt-5" variant='text' height={48} />
-                <Skeleton sx={{ bgcolor: 'grey.900' }} variant="rectangular" width={100} height={38} />
-            </div> 
-            )}
-            { error && (
-                <div>
-                    {error}
-                </div>
-            )}
-            {  data &&
-                <TripTable data={data} />
+            { addTripFormOpen ? <AddTripForm/> : <TripTable/>}
+            </div>
+            { !addTripFormOpen ? 
+                    <div className='trip-modal-footer'>
+                        <button className="btn btn-dm-white" onClick={addTripOnClickListener}>Add trip</button>
+                    </div> : 
+                    <div className='trip-modal-footer'>
+                     <button className="btn btn-dm-white rounded-lg" onClick={backTripModalListener}>Back</button>
+                    </div>
             }
-            </div>
-            <div className='trip-modal-footer'>
-                <button className="btn btn-dm-white" onClick={addTripOnClickListener}>Add trip</button>
-            </div>
         </div>
     )
 }
